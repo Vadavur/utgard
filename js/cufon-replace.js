66a4f2ca67520c5b70f2d7096c87f1487db9b0d5
Давыд Васильцов <65164004+Vadavur@users.noreply.github.com>
2021-02-26 13:27:57 +0300

    Add files via upload

---
+++ js/cufon-replace.js
@@ -0,0 +1,8 @@
+Cufon.replace('#header .row-2 ul li', { fontFamily: 'Tahoma', textShadow:'#397ece 1px 1px' });
+Cufon.replace('h2, .maininfo', { fontFamily: 'Tahoma', color: '-linear-gradient(#fff, #fff, #fff, #fff, #fff, #fff, #cccccc, #cccccc, #cccccc, #cccccc, #cccccc)' });
+Cufon.replace('b, .bottom, .phonenumber', { fontFamily: 'Tahoma', color: '-linear-gradient(#3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #3b98dd, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2, #1f7dd2)' });
+Cufon.replace('#search-form label', { fontFamily: 'Tahoma' });
+Cufon.replace('.link1', { fontFamily: 'Tahoma', textShadow:'#165ebd 1px 1px' });
+Cufon.replace('h3, h4', { fontFamily: 'Tahoma' });
+Cufon.replace('h3 b, h4 b', { fontFamily: 'Tahoma' });
+Cufon.replace('.box2 h4, .box3 h4', { fontFamily: 'Tahoma', textShadow:'#2074cb 1px 1px' });
---
+++ js/owl.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    items:3,
+    autoWidth:true,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true,
+    autoplaySpeed:2500,
+    fluidSpeed:true
+});
+});
